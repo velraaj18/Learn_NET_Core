11f1ed5da8cfc8ef6d3f37904c73ea4546c9ed53
Velraaj <velraaj30@gmail.com>
2026-02-24 03:33:10 +0530

    Modified git ignore file

--- FirstAPI/Program.cs
+++ FirstAPI/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 // Add services for dependency injection
 builder.Services.AddScoped<WeatherService>();
@@ -15,10 +14,6 @@ builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(buil
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.MapControllers();
